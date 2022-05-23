44307b0b8cff38e5951628d212450c056de1e91f
precious <chineduprecious47@mail.com>
2022-05-23 13:05:16 +0100

    update

--- index.js
+++ index.js
@@ -10,15 +10,14 @@ const debug = require('debug')('app:startUp');
 // Setup
 connectDB();
 appRoutes(app);
-// jobs(); 
-
+// jobs();
 
 // Get Node Environment
 debug(`ENV: ${app.get('env')}`);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
-    // TODO: uncomment this
-    // debug(`Listening on port ${port}`);
-     console.log(`Listening on port ${port}`);
+  // TODO: uncomment this
+  // debug(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
